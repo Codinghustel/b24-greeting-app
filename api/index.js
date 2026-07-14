2306c5aefae90b91401e0850058d1e5dafcc8202
Asad <muhammadasadullahshahid30@gmail.com>
2026-07-14 12:59:37 +0500

    Fix template.html path reference

--- api/index.js
+++ api/index.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-  const htmlPath = path.join(process.cwd(), 'index.html');
+  const htmlPath = path.join(process.cwd(), 'template.html');
   const html = fs.readFileSync(htmlPath, 'utf8');
-
   res.setHeader('Content-Type', 'text/html');
   res.status(200).send(html);
 };
